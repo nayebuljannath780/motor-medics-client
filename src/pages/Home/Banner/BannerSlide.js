6eecc631cc181df01afebcfaef40a3e94bb9534b
nayebuljannath780 <chynayeb780@gmail.com>
2023-04-22 23:45:36 +0600

    some changes in slide

---
+++ src/pages/Home/Banner/BannerSlide.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './BannerSlide.css'
+
+const BannerSlide = ({slide}) => {
+    const {image, id, next, prev} = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+                <div className='carousel-img'>
+                    <img src={image} className="w-full rounded-xl" alt='' />
+                </div>
+                <div className="absolute flex transform -translate-y-1/2 left-5 top-1/4 m mx-10">
+                    <h1 className='text-6xl text-yellow-400 font-semibold'>Providing <br />top notch service <br /> in reasonable price</h1>
+                </div>
+                <div className="absolute flex transform -translate-y-1/2 left-5 top-2/4 m mx-10">
+                    <h1 className='text-xl text-slate-300 w-1/2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet mauris a tortor aliquet aliquet ac in turpis. Integer ac mauris in neque lacinia aliquet non fermentum justo</h1>
+                </div>
+                <div className="absolute flex transform -translate-y-1/2 left-5 top-3/4 m mx-10">
+                    <button className="btn btn-warning mr-5">Button</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 mx-10">
+                    <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                    <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+                </div>
+            </div>     
+    );
+};
+
+export default BannerSlide;
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   ]
 
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto py-2'>
       <div className="navbar h-5 bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
